a110b96222d322b6aa7ab895e252f7cfe80176e0
ksamuk <ksamuk@gmail.com>
2015-03-06 13:15:09 -0800

    updating ds script

--- ev_prep_scripts/build_ds_data_table.R
+++ ev_prep_scripts/build_ds_data_table.R
@@ -1,21 +1,38 @@
 ####create data table from (pre-processed) codeml output
-####
+####outputs gene ids and ds values for gacu ONLY
+####KS mar-3-2015
 
-library(ap)ea
+library(ape)
 
-home.dir<-"E:/Genome Meta Analysis/ev_prep_scripts/paml_analysis"
+#home dir set up
+#home.dir<-"E:/Genome Meta Analysis/ev_prep_scripts/paml_analysis"
+#home.dir<-"~/Documents/Science/Projects/Ph.D./Genome Meta Analysis/ev_prep_scripts/paml_analysis"
+
 setwd(home.dir)
 
-file.con<-file("ENSGACP00000014140.cml.txt")
-file.lines<-readLines(file.con)
+#contained pre-processed paml output (the dn and ds trees)
+paml.output.dir<-""
 
-ds.tree<-read.tree(text=file.lines[2])
+#dat file list
+file.list<-list.files(paml.output.dir)
 
-plot(ds.tree)
+#set up empty output dataframe
+gene.id<-vector(mode="character",length=length(file.list))
+ds<-vector(mode="numeric",length=length(file.list))
+gacu.ds<-data.frame(gene.id,ds)
 
-gacu.edge<-which.edge(ds.tree, c("ENSGACP00000014140"))
+for (i in 1:length(file.list)){
+  
+  file.con<-file(file.list[i])
+  file.lines<-readLines(file.con)
+  ds.tree<-read.tree(text=file.lines[2])
+  
+  gene.name<-sapply(strsplit(file.list[i],split=".cml.txt"),function(x)x[1])
+  gacu.ds$gene.id[i]<-gene.name
+  gacu.ds$ds[i]<-ds.tree$edge.length[which.edge(ds.tree, gene.name)]
+  if (i%%1000==0){
+    print(i)
+  }
+}
 
-ds.tree$edge.length[gacu.edge]
-ds.
 
-branchlist <- 
