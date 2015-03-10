908f3834e3c53ee7782b93b1af0124e2bd9fa463
ksamuk <ksamuk@gmail.com>
2015-03-10 14:39:59 -0700

    finishing script

--- ev_prep_scripts/build_ds_data_table.R
+++ ev_prep_scripts/build_ds_data_table.R
@@ -2,15 +2,28 @@
 ####outputs gene ids and ds values for gacu ONLY
 ####KS mar-3-2015
 
+########HEAD
+
 library(ape)
+library(biomaRt)
+library(dplyr)
+library(magrittr)
 
 #home dir set up
 #home.dir<-"E:/Genome Meta Analysis/ev_prep_scripts/paml_analysis"
 home.dir<-"~/Documents/Science/Projects/Ph.D./Genome Meta Analysis/ev_prep_scripts/paml_analysis"
 #home.dir<-"~/review/analysis/gma/ev_prep_scripts/paml_analysis"
 
-#contained pre-processed paml output (the dn and ds trees)
+#contains pre-processed paml output (the dn and ds tree lines+first file of paml file)
 paml.output.dir<-file.path(home.dir,"output")
+
+#the output director (window evs)
+out.dir<-"~/Documents/Science/Projects/Ph.D./Genome Meta Analysis/evs/window"
+
+########END HEAD
+
+########PROCESS PAML OUTPUT FILES
+
 setwd(paml.output.dir)
 
 #dat file list
@@ -20,8 +33,7 @@ file.list<-list.files()
 gene.id<-vector(mode="character",length=length(file.list))
 ds<-vector(mode="numeric",length=length(file.list))
 dn<-vector(mode="numeric",length=length(file.list))
-num.species<-vector(mode="numeric",length=length(file.list))
-gacu.ds<-data.frame(gene.id,ds,dn,num.species,stringsAsFactors=FALSE)
+gacu.ds<-data.frame(gene.id,ds,dn,stringsAsFactors=FALSE)
 
 for (i in 1:length(file.list)){
   
@@ -33,7 +45,6 @@ for (i in 1:length(file.list)){
   if (!is.na(file.lines[2])){  
     
     #make the tree(s)
-    num.species<-substr(gsub(" ","",file.lines[1]),1,1)
     ds.tree<-read.tree(text=file.lines[3])
     dn.tree<-read.tree(text=file.lines[5])
     
@@ -56,8 +67,6 @@ for (i in 1:length(file.list)){
       gacu.ds$dn[i]<-dn.tree$edge.length[which.edge(dn.tree,gene.name)]
     }
     
-    gacu.ds$num.species[i]<-num.species
-    
     #tracker
     if (i%%1000==0){
       cat(i,"...",sep="")
@@ -68,14 +77,51 @@ for (i in 1:length(file.list)){
     gacu.ds$gene.id[i]<-sapply(strsplit(file.list[i],split=".cml.txt"),function(x)x[1])
     gacu.ds$ds[i]<-NA
     gacu.ds$dn[i]<-NA
-    gacu.ds$num.species<-NA
   }
   
 }
 
 gacu.ds<-gacu.ds[complete.cases(gacu.ds),]
 
-#output
-setwd(home.dir)
-write.table(gacu.ds,file="ds_estimates_gacu.txt")
+########END PROCESS PAML OUTPUT FILES
 
+#####match gene.ids to genomic coordinates
+
+#initialize gacu ensembl
+ensembl <- useMart("ensembl",dataset="gaculeatus_gene_ensembl")
+
+#the attributes of interest
+attributes.feat<-c("ensembl_gene_id",
+                  "ensembl_peptide_id",
+                   "start_position",
+                   "end_position",
+                   "chromosome_name")
+
+#query ensembl
+coords<-getBM(attributes=attributes.feat,values=gacu.ds$gene.id,filters=c("ensembl_peptide_id"),mart = ensembl)
+
+########CLEAN FOR OUTPUT
+
+#the output dataframe
+gacu.out<-data.frame(lg=coords$chromosome_name,
+                    pos1=coords$start_position,
+                    pos2=coords$end_position,
+                    ds=gacu.ds$ds,
+                    dn=gacu.ds$dn)
+
+#remove scaffolds
+gacu.out.2<-gacu.out[grep("scaff*",gacu.out$lg,invert=TRUE),]
+#convert lg to numeric (!)
+gacu.out.2$lg<-as.numeric(as.roman(sapply(strsplit(as.character(gacu.out.2$lg),"group"),function(x)x[2])))
+#arrange
+gacu.out.2<-arrange(gacu.out.2,lg)
+
+gacu.out.ds<-gacu.out.2[,1:4]
+gacu.out.dn<-gacu.out.2[,c(1:3,5)]
+
+#output
+setwd(out.dir)
+write.table(gacu.out.ds,file="ds.txt",row.names=FALSE)
+write.table(gacu.out.dn,file="dn.txt",row.names=FALSE)
+
+#########END CLEAN FOR OUTPUT
