43ed49c1d7b6d29e75431f8b1bc215199c48d174
ksamuk <ksamuk@gmail.com>
2015-03-24 15:03:25 -0700

    gene density calculations

--- ev_prep_scripts/gene_density_calc_build.R
+++ ev_prep_scripts/gene_density_calc_build.R
@@ -9,6 +9,8 @@ library(biomaRt)
 library(dplyr)
 library(data.table)
 
+########BIOMART: GENE LOCATIONS
+
 #set the biomart
 gacu.ensembl <- useMart("ensembl",dataset="gaculeatus_gene_ensembl")
 
@@ -30,18 +32,50 @@ gene.list<-arrange(gene.list,chromosome_name,start_position)
 #standard formatting
 gene.list<-data.frame(lg=gene.list$chromosome_name,pos1=gene.list$start_position,pos2=gene.list$end_position)
 
-#combine duplicate rows in feature dataset
+#outputs ev files (ev joining script sorts out % density calculation automagically)
 
-DT <- data.table(gene.list, key="ensembl_gene_id")
-DT2<-DT[, list(chromosome_name=chromosome_name,
-               start_position=start_position, 
-               end_position=end_position, 
-               gene_biotype=gene_biotype,
-               go_ids=list(go_id)),
-        by=ensembl_gene_id]
+gene.list.out<-gene.list
+gene.list.out$gene.density<-1
 
-go.df<-data.frame(ensembl_gene_id=DT$ensembl_gene_id,go_id=DT$go_id)
+#write to file
+write.table(gene.list.out,file=file.path(getwd(),"evs","gene_density.txt"),row.names=FALSE)
 
-#collapse duplicated rows
-master.list.feat.collapsed<-data.frame(DT2)
-master.list.feat.collapsed<-master.list.feat.collapsed[!duplicated(master.list.feat.collapsed$ensembl_gene_id),]
+########END BIOMART: GENE LOCATIONS
+
+########SLIDING WINDOWS OF GENE COUNT
+
+window.size<-75000
+
+#kind of a grody implementation
+
+gene.counts.chr<-list()
+for (i in 1:max(gene.list$lg)){
+  gene.list.chr<-subset(gene.list,gene.list$lg==i)
+  
+  #convert nuc position to window index
+  window.nums<-as.integer(gene.list.chr$pos1/window.size)+1
+  
+  #tally up genes in windows
+  window.names<-as.numeric(names(table(window.nums)))
+  window.counts<-as.numeric(table(window.nums))
+  
+  #reformatting 
+  gene.counts.chr[[i]]<-data.frame(win.num=window.names,gene.count=window.counts)
+  
+  #calc window boundaries
+  window.pos1<-(gene.counts.chr[[i]]$win.num-1)*window.size
+  window.pos1[window.pos1==0]<-1
+  window.pos2<-((gene.counts.chr[[i]]$win.num)*window.size)-1
+  
+  #the output for this chromo
+  lg<-rep(i,length(window.pos2))
+  gene.counts.chr[[i]]<-data.frame(lg=lg,pos1=window.pos1,pos2=window.pos2,gene.count=window.counts)
+}
+
+#bind
+gene.counts.out<-do.call("rbind",gene.counts.chr)
+
+#write to file
+write.table(gene.counts.out,file=file.path(getwd(),"evs","gene_count.txt"),row.names=FALSE)
+
+########END SLIDING WINDOWS OF GENE COUNT
