f7ad2e528fe160065de8b746bd639972ac713d35
ksamuk <ksamuk@gmail.com>
2015-06-17 21:51:26 -0700

    parallel processing for null dist calculation

--- 3.4_outlier_clustering.R
+++ 3.4_outlier_clustering.R
@@ -3,6 +3,8 @@
 library(data.table)
 library(dplyr)
 library(ggplot2)
+library(microbenchmark)
+library(parallel)
 
 
 # read in snp file
@@ -38,6 +40,8 @@ snp.not.outliers <- snp.file %>%
 # calc distances between outliers
 
 dist.outliers <- data.frame()
+
+# Old distance method -----------------------------------------------------
 
 for (i in unique(snp.outliers$study_com)){
   print(paste0("processing ", i,"..."))
@@ -91,100 +95,8 @@ dist.outliers$geography <- sapply(dist.outliers$study_com,add.geo)
 
 dist.outliers$outlier <- TRUE
 
-################### NOT OUTLIERS
 
-dist.not.outliers <- data.frame()
-
-for (i in unique(snp.outliers$study_com)){
-  print(paste0("processing ", i,"..."))
-  snp.outlier.sub <- subset(snp.not.outliers , snp.not.outliers$study_com == i)
-  diff.sub <- data.frame()
-  
-  for (j in unique(snp.outlier.sub$lg)){
-    snp.outlier.sub.lg <- snp.outlier.sub %>%
-      filter(lg == j) %>%
-      arrange(gen.pos)
-    
-    diff.lg <- data.frame(outlier.dist = diff(snp.outlier.sub.lg$gen.pos))
-    if (length(diff.lg$outlier.dist)>0){
-      diff.lg$lg <- j
-      diff.sub <- rbind(diff.sub, diff.lg)
-    }
-    
-  }
-  
-  diff.sub$study <- snp.outlier.sub$study[1]
-  diff.sub$comparison <- snp.outlier.sub$comparison[1]
-  diff.sub$study_com <- snp.outlier.sub$study_com[1]
-  
-  dist.not.outliers <- rbind(dist.not.outliers, diff.sub)
-  
-}
-
-# inject geography
-
-dist.not.outliers$geography <- sapply(dist.not.outliers$study_com,add.geo)
-
-dist.not.outliers$outlier <- FALSE
-
-dist.snps <- rbind(dist.outliers, dist.not.outliers)
-
-dist.snps <- dist.snps %>%
-  arrange(study_com, lg)
-
-# bind snp distance types
-
-
-##################### PLOTS
-
-  
-dist.snps  %>%
-  ggplot(aes(x = study_com, y = outlier.dist, fill = geography, color = outlier)) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
-  facet_grid(~geography, scale="free_x")
-
-snp.dist.summary %>%
-  ggplot(aes(x = study_com, y = dispersion, color = geography)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-dist.snps  %>%
-  ggplot(aes(x = geography, y = log(outlier.dist), fill = outlier)) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-
-
-############### NEW APPROACH
-
-dist.distributions <- data.frame()
-
-for (i in unique(snp.outliers$study_com)){
-  print(paste0("processing ", i,"..."))
-  snp.outlier.sub <- subset(snp.not.outliers , snp.not.outliers$study_com == i)
-  diff.sub <- data.frame()
-  
-  for (j in unique(snp.outlier.sub$lg)){
-    snp.outlier.sub.lg <- snp.outlier.sub %>%
-      filter(lg == j) %>%
-      arrange(gen.pos)
-    
-    diff.lg <- data.frame(outlier.dist = diff(snp.outlier.sub.lg$gen.pos))
-    if (length(diff.lg$outlier.dist)>0){
-      diff.lg$lg <- j
-      diff.sub <- rbind(diff.sub, diff.lg)
-    }
-    
-  }
-  
-  diff.sub$study <- snp.outlier.sub$study[1]
-  diff.sub$comparison <- snp.outlier.sub$comparison[1]
-  diff.sub$study_com <- snp.outlier.sub$study_com[1]
-  
-  dist.not.outliers <- rbind(dist.not.outliers, diff.sub)
-  
-}
+############### NEW APPROACH: PERMUTATION FTW
 
 
 permute_distances <- function(x, num.samples){
@@ -220,17 +132,23 @@ permute_distances <- function(x, num.samples){
 }
 
 
-sample_sites <- . %>% sample_n(num.outliers) %>% arrange(gen.pos) %>% select(gen.pos) %>% with(as.numeric(gen.pos)) %>% mean(na.rm = TRUE)
+sample_sites <- function (gen.pos, num.outliers) {
+  
+  site.sample <- gen.pos %>% sample_n(num.outliers) %>% arrange(gen.pos) %>% select(gen.pos) %>% with(as.numeric(gen.pos)) %>% mean(na.rm = TRUE)
+  return(site.sample)
+  
+}
   
 permute_distances_snp <- function(x, num.samples){
   
-  mean.distances.df <- data.frame()
+  mean.distances.df <- list()
   
   for (i in unique(x$study_com)){
     
     print(paste0("processing ", i,"..."))
     dist.sub <- subset(x, x$study_com == i)
-    mean.distances.lg.all <- data.frame()
+    mean.distances.lg.all <- list()
+    
     for (j in unique(snp.outlier.sub$lg)){
       
       dist.sub.lg <- dist.sub %>%
@@ -242,28 +160,55 @@ permute_distances_snp <- function(x, num.samples){
       
       if (num.outliers > 0){
         
-        mean.distances.lg <- replicate(num.samples, sample_sites(dist.sub.lg))
-        
+        mean.distances.lg <- replicate(num.samples, sample_sites(dist.sub.lg, num.outliers))
         mean.distances.lg.df <- data.frame(mean.distance = mean.distances.lg)
         mean.distances.lg.df$lg <- j
-        
-        mean.distances.lg.all <- rbind(mean.distances.lg.all, mean.distances.lg.df)
+        mean.distances.lg.all[[j]] <- mean.distances.lg.df
       }
       
     }
+    mean.distances.lg.all <- do.call("rbind", mean.distances.lg.all)
     mean.distances.lg.all$study_com = i
-    mean.distances.df <- rbind(mean.distances.df, mean.distances.lg.all)
+    mean.distances.df[[i]] <- mean.distances.lg.all
     
   }
-  
+  mean.distances.df <- do.call("rbind", mean.distances.df)
+  rownames(mean.distances.df) <- NULL
   return(mean.distances.df)
   
 }
 
-distance.null <- permute_distances_snp(snp.file, 10)
+distances.null <- permute_distances_snp(snp.file, 1000)
 
-distance.null %>%
-  filter(study_com == "benlim_lq") %>%
-  ggplot(aes(x = mean.distance)) +
-  geom_histogram()+
-  facet_wrap(~lg)
+permute_distances_snp_list <- function(x, num.samples){
+  
+  mean.distances.lg.all <- list()  
+  
+    for (j in unique(x$lg)){
+      
+      dist.sub.lg <- x %>%
+        filter(lg == j) %>%
+        filter(!is.na(gen.pos)) %>%
+        arrange(gen.pos)
+      
+      num.outliers <- sum(as.numeric(dist.sub.lg$fst.outlier), na.rm = TRUE)
+      
+      if (num.outliers > 0){
+        
+        mean.distances.lg <- replicate(num.samples, sample_sites(dist.sub.lg, num.outliers))
+        mean.distances.lg.df <- data.frame(mean.distance = mean.distances.lg)
+        mean.distances.lg.df$lg <- j
+        mean.distances.lg.all[[j]] <- mean.distances.lg.df
+      }
+      
+    }
+    mean.distances.lg.all <- do.call("rbind", mean.distances.lg.all)
+    mean.distances.lg.all$study_com = i
+    return(mean.distances.lg.all)
+  
+}
+
+split.df <- split(snp.file, snp.file$study_com)  
+mclapply(split.df, permute_distances_snp_list, num.samples = 10)
+
+
