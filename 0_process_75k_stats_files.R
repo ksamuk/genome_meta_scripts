1e6a14cd052c68a85a29dcaf0ffe318cb28bd0c1
ksamuk <ksamuk@gmail.com>
2015-08-19 09:35:24 -0700

    regions update for permutation

--- 0_process_75k_stats_files.R
+++ 0_process_75k_stats_files.R
@@ -212,14 +212,37 @@ write.table(coeff.dat, file = "analysis_ready/75k_stats_model_fst_fits.txt", row
 ## can start fresh here
 rm(list=ls())
 coeff.dat <- read.table(file = "analysis_ready/75k_stats_model_fits.txt", header = TRUE, stringsAsFactors = FALSE)
-
 coeff.dat <- read.table(file = "analysis_ready/75k_stats_model_fst_fits.txt", header = TRUE, stringsAsFactors = FALSE)
+
+## add in region data (for looser geography)
+region.dat <- read.table(file = "population_regions.txt", header = TRUE, stringsAsFactors = FALSE)
+
+# make short pop codes
+region.dat$pop.code <- strsplit(region.dat$pop.code, split = "_") %>% lapply(function(x)return(x[1])) %>% unlist
+
+# associate pops in coeff dat with regions in region dat
+region.sub <- region.dat[,c(1,4)]
+names(region.sub) <- c("pop1","reg1")
+coeff.dat$reg1 <- region.sub$reg1[match(coeff.dat$pop1, region.sub$pop1)]
+
+region.sub <- region.dat[,c(1,4)]
+names(region.sub) <- c("pop2","reg2")
+coeff.dat$reg2 <- region.sub$reg2[match(coeff.dat$pop2, region.sub$pop2)]
+
+# make new geographic categories
+coeff.dat$geography2 <- ifelse(coeff.dat$reg1==coeff.dat$reg2, "para", "allo")
+
+#make new groups :o
+coeff.dat$group2 <- paste0(coeff.dat$geography2,"_",coeff.dat$ecology)
+
 
 ############################## DO ANY GROUPS DIFFER FROM THE "STRONG NULL"
 
 #permute means
 coeff.dat.small <- coeff.dat %>%
-  select(group, recomb_rate)
+  select(group2, recomb_rate)
+
+names(coeff.dat.small)[1] <- "group"
  
 # function that shuffles groups, calculates their mean recom coeff, and returns the latter
 permute_means <- function(data) {
